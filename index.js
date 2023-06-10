aa7075cec8568066ebc0f393d6fee418687e9053
helheim254@gmail.com <MM@Reginas-MacBook-Air.local>
2023-06-10 15:37:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Martin is the champion';
+newHeader.append();
